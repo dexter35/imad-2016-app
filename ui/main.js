c563f67818d237f9d3cc0f4824cd42d3d5730533
dexter35 <arpitch35@gmail.com>
2016-11-06 18:28:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+var button=getElementById('counter');
+var counter=0;
+button.onClick =function()
+{
+  countercounter+1;
+  var span=document.getElementById('count');
+  innerHTML=counter.toString();
+};
