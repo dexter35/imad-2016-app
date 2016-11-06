b9380360eb0dd118d5cb05292e46712ed59534f5
dexter35 <arpitch35@gmail.com>
2016-11-06 18:37:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ var button=getElementById('counter');
 var counter=0;
 button.onClick =function()
 {
-  countercounter+1;
+  counter=counter+1;
   var span=document.getElementById('count');
   span.innerHTML=counter.toString();
 };
